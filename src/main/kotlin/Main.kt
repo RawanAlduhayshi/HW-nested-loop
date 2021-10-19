4ddf70af6e4cd3241b39e5e16529a654938d1362
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-19 19:27:57 +0300

    nested loop

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Enter an Integer number")
+  var num = readLine()!!.toInt()
+
+    for (index in 1..num){
+       // println("$index*$index")
+        println("")
+        for (index2 in 1..index){
+            print("1*$index2")
+            print("\t")}
+
+    }
+
+
+}
